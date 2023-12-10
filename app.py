635e08443b3692ff66575cb6c48b3855947bde5d
rayandu924 <rayandu924@gmail.com>
2023-12-10 19:26:36 +0100

    Summary (required)

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
+# MySQL Configuration
+mysql_config = {
+    'host': 'localhost', # Replace with your MySQL host IP address
+    'user': 'user',     # Replace with your MySQL username
+    'password': 'password', # Replace with your MySQL password
+    'database': 'healer'        # Replace with your MySQL database name
+}
+
+# Create a MySQL connection
+conn = mysql.connector.connect(host=mysql_config['host'],
+                               user=mysql_config['user'],
+                               password=mysql_config['password'],
+                               database=mysql_config['database'])
+
 @app.route('/')
 def index():
-    return "Bienvenue sur mon serveur Flask connecté à MySQL!"
 
 if __name__ == '__main__':
     app.run(debug=True)
